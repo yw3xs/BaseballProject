5dd1bdb61f7f771986e581310405efb4b7c45002
aaron <robertsaj2@gmail.com>
2014-06-11 14:43:30 -0400

    script and urls added to create db of players

--- BaseballProjectRework/HTMLGrab/HTMLGrab.py
+++ BaseballProjectRework/HTMLGrab/HTMLGrab.py
@@ -39,44 +39,86 @@ def getTableData(table):
     return title, col_heads, data_rows
 
 
-
-#url = 'http://espn.go.com/mlb/player/stats/_/id/30836/mike-trout'
-url = 'http://espn.go.com/mlb/player/stats/_/id/6194/felix-hernandez'
-clean_url = re.sub('[%s]' % re.escape(string.punctuation), '', url)
-response = urllib2.urlopen(url)
-html = response.read()
-
-soup = BeautifulSoup(html)
-
-tables = soup('table', {'class' : 'tablehead'})
-header_stat_table = soup.find('table', {'class' : 'header-stats'})
+def dbInsert(base_url):
+	"""Insert data into a mongodb instance
+	
+	this will take the base_url and creat the url where the stats are contained,
+	it will get the stats and create a document for mongodb
+	"""
+	
+	url = base_url.replace('player/','player/stats/') # stats are here
+	clean_url = re.sub('[%s]' % re.escape(string.punctuation), '', base_url) # use as _id
+	
+	# get the tables containing stats using beautiful soup
+	response = urllib2.urlopen(url)
+	html = response.read()
+	soup = BeautifulSoup(html)
+	tables = soup('table', {'class' : 'tablehead'})
 
 	
-conn = pymongo.MongoClient()
-db = conn.PlayerStats
+	
 
-player_dict = {}
+	player_dict = {}
 
-for table in tables:    
+	for table in tables:    
     
-    title, col_heads, data_rows = getTableData(table)
+		title, col_heads, data_rows = getTableData(table)
     
-    season_dict = {}
-    for row in data_rows:
-    	data_dict = {}
-    	for col in range(len(row)):
-    		data_dict[col_heads[col]] = row[col]
+		season_dict = {}
+		for row in data_rows:
+			data_dict = {}
+			for col in range(len(row)):
+				data_dict[col_heads[col]] = row[col]
     	
-    	season_dict[str(data_dict['SEASON'])] = data_dict
-    
-    player_dict[title] = season_dict
+			season_dict[str(data_dict[col_heads[0]])] = data_dict
+
+    	# there are two tables called 'MISCELLANEOUS', this block makes sure
+    	# the second one doesn't simply replace the first in the dict
+		try: 
+			for season in season_dict.keys():
+				if ((season != 'Total') & (season != 'Season Averages')):
+					player_dict[title][season].update(season_dict[season])
+		except:
+			player_dict[title] = season_dict
     
 
-player_dict['_id'] = clean_url
+	# find out if player is a hitter or pitcher
+	base_response = urllib2.urlopen(base_url)
+	base_html = base_response.read()
+	base_soup = BeautifulSoup(base_html)
+	header_stat_table = base_soup.find('table', {'class' : 'header-stats'})
+	player_type = playerType(str(header_stat_table))
+
+	# create the rest of the player_dict entries
+	player_dict['player_type'] = player_type
+	player_dict['base_url'] = base_url
+	player_dict['_id'] = clean_url
+	player_dict['name'] = base_url.split('/')[-1].replace('-', ' ') # get player name from end of url
     	
-db.Players.insert(player_dict)
+    	
+    # put document in mongodb instance	
+	conn = pymongo.MongoClient()
+	db = conn.PlayerStats
+	db.Players.insert(player_dict)
 
 
+# loop over all players in urlset
+
+with open('UrlSet.txt', 'r') as file:
+	lines = file.readlines()
+
+num_lines = len(lines)
+line_num = 1
+
+for line in lines:	
+	# print out progress
+	print 'processing url ' + str(line_num) + ' out of ' + str(num_lines)
+	dbInsert(line.strip('\n').strip('"'))	
+	line_num += 1
+
+#dbInsert('http://espn.go.com/mlb/player/_/id/30836/mike-trout')
+#dbInsert('http://espn.go.com/mlb/player/_/id/6194/felix-hernandez')
+#dbInsert('http://espn.go.com/mlb/player/_/id/1720/ruben-sierra')
 
 
 
